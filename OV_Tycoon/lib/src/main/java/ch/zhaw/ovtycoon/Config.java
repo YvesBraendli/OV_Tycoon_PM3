f9ffec0a1604aaf16ca20a23b4fe4f35fa791466
braenyve <braenyve@students.zhaw.ch>
2022-04-16 11:51:03 +0200

    Added the enums Regions and Zones

--- OV_Tycoon/lib/src/main/java/ch/zhaw/ovtycoon/Config.java
+++ OV_Tycoon/lib/src/main/java/ch/zhaw/ovtycoon/Config.java
@@ -7,4 +7,65 @@ public class Config {
 	public static final int MIN_ATTACKERS = 1;
 	public static final int REGION_BONUS = 2;
 	
+	enum Regions{
+		Zurich,
+		Winterthur,
+		Horgen,
+		Affoltern,
+		Andelfingen,
+		Bulach,
+		Dielsdorf,
+		Dietikon,
+		Hinwil,
+		Meilen,
+		Pfaeffikon,
+		Uster
+	}
+	
+	enum Zones{
+		Zone110,
+		Zone111,
+		Zone112,
+		Zone113,
+		Zone114,
+		Zone115,
+		Zone116,
+		Zone117,
+		Zone118,
+		Zone120,
+		Zone121,
+		Zone122,
+		Zone123,
+		Zone124,
+		Zone130,
+		Zone131,
+		Zone132,
+		Zone133,
+		Zone134,
+		Zone135,
+		Zone140,
+		Zone141,
+		Zone142,
+		Zone143,
+		Zone150,
+		Zone151,
+		Zone152,
+		Zone153,
+		Zone154,
+		Zone155,
+		Zone156,
+		Zone160,
+		Zone161,
+		Zone162,
+		Zone163,
+		Zone164,
+		Zone170,
+		Zone171,
+		Zone172,
+		Zone173,
+		Zone180,
+		Zone181,
+		Zone184
+	}
+	
 }
