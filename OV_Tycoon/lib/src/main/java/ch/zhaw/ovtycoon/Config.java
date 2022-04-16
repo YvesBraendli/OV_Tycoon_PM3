1cd1ec2d0df429ed531cabb936d90ae98e94ed9f
braenyve <braenyve@students.zhaw.ch>
2022-04-16 11:31:59 +0200

    Added all public static final values

--- OV_Tycoon/lib/src/main/java/ch/zhaw/ovtycoon/Config.java
+++ OV_Tycoon/lib/src/main/java/ch/zhaw/ovtycoon/Config.java
@@ -2,6 +2,9 @@ package ch.zhaw.ovtycoon;
 
 public class Config {
 	public static final int MAX_DICE_VALUE = 6;
-	public static final int MAX_THROWABLE_DICE = 200;
+	public static final int MAX_THROWABLE_DICE = 3;
+	public static final int MAX_ATTACKERS = 3;
+	public static final int MIN_ATTACKERS = 1;
+	public static final int REGION_BONUS = 2;
 	
 }
