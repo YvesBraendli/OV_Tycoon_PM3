916cd8598ccaf003e981152251173fd52f81e25a
meierr02 <meierr02@students.zhaw.ch>
2022-05-04 19:47:10 +0200

    45 moved application away from interface

--- lib/src/main/java/ch/zhaw/ovtycoon/Main.java
+++ lib/src/main/java/ch/zhaw/ovtycoon/Main.java
@@ -1,11 +1,26 @@
 package ch.zhaw.ovtycoon;
 
+import ch.zhaw.ovtycoon.gui.MapController;
 import ch.zhaw.ovtycoon.model.Game;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main {
 
     public static void main(String[] args) {
-        Application.launch(RisikoController.class, args);
+        Application.launch(App.class, args);
+    }
+    
+    public static class App extends Application{
+        @Override
+        public void start(Stage stage) throws Exception {      	
+            FXMLLoader fxmlLoader = new FXMLLoader(MapController.class.getClassLoader().getResource("zones-map-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
+            stage.setTitle("OV-Tycoon");
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 }
--- lib/src/main/java/ch/zhaw/ovtycoon/RisikoController.java
+++ lib/src/main/java/ch/zhaw/ovtycoon/RisikoController.java
@@ -13,21 +13,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class RisikoController extends Application {
+public class RisikoController{
 
 	private Game game;
 	
-    @Override
-    public void start(Stage stage) throws Exception {
-    	game = new Game();
+	public RisikoController() {
+		game = new Game();
     	game.initGame(2);
-    	
-        FXMLLoader fxmlLoader = new FXMLLoader(MapController.class.getClassLoader().getResource("zones-map-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
-        stage.setTitle("OV-Tycoon");
-        stage.setScene(scene);
-        stage.show();
-    }
+	}
+
     
     public int[][] getLastRolledDie(){
     	return game.getLastRolledDie();
