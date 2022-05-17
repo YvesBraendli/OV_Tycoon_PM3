e569fa6b6d2955fc3360d2f7f268bcca3561539a
braenyve <braenyve@students.zhaw.ch>
2022-05-17 07:46:43 +0200

    removed unused code fragments/ unnecessary comments and reformated classes

--- lib/src/main/java/ch/zhaw/ovtycoon/model/Region.java
+++ lib/src/main/java/ch/zhaw/ovtycoon/model/Region.java
@@ -2,18 +2,19 @@ package ch.zhaw.ovtycoon.model;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+
 import ch.zhaw.ovtycoon.Config.RegionName;
 
 public class Region implements Serializable {
-	private RegionName name;
-	private ArrayList<Zone> zones = new ArrayList<Zone>();
-	
-	public Region(RegionName name, ArrayList<Zone> zones ) {
-		this.name = name;
-		this.zones = zones;
-	}
-	
-	public ArrayList<Zone> getZones() {
-		return zones;
-	}
+    private RegionName name;
+    private ArrayList<Zone> zones;
+
+    public Region(RegionName name, ArrayList<Zone> zones) {
+        this.name = name;
+        this.zones = zones;
+    }
+
+    public ArrayList<Zone> getZones() {
+        return zones;
+    }
 }
--- lib/src/main/java/ch/zhaw/ovtycoon/model/Zone.java
+++ lib/src/main/java/ch/zhaw/ovtycoon/model/Zone.java
@@ -5,67 +5,52 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Zone implements Serializable {
-	private String name;
-	private int troops;
-	private ArrayList<Zone> neighbours;
-	private boolean alreadyVisited;
+    private String name;
+    private int troops;
+    private ArrayList<Zone> neighbours;
 
-	public Zone(String name) {
-		this.name = name;
-		neighbours = new ArrayList<Zone>();
-		troops = 0;
-		alreadyVisited = false;
-	}
+    public Zone(String name) {
+        this.name = name;
+        neighbours = new ArrayList<Zone>();
+        troops = 0;
+    }
 
-	public void addTroops(int amount){
-		this.troops += amount;
-	}
+    public void addTroops(int amount) {
+        this.troops += amount;
+    }
 
-	public boolean getAlreadyVisited(){
-		return alreadyVisited;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setAlreadyVisited(boolean alreadyVisitedState){
-		alreadyVisited = alreadyVisitedState;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void decreaseZone(int amount) {
-		troops -= amount;
-	}
-	
-	public int getTroops() {
-		return troops;
-	}
+    public void decreaseZone(int amount) {
+        troops -= amount;
+    }
 
-	public void setTroops(int troops) {
-		this.troops = troops;
-	}
-	
-	public void setNeighbours(ArrayList<Zone> neighbours) {
-		this.neighbours = neighbours;
-	}
-	
-	public ArrayList<Zone> getNeighbours(){
-		return neighbours;
-	}
-	
-	public boolean isNeighbour(Zone zone) {
-		if(neighbours.contains(zone)) return true;
-		return false;
-	}
+    public int getTroops() {
+        return troops;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if(this == o) return true;
-		if(o == null) return false;
-		if (getClass() != o.getClass()) return false;
-		Zone zone = (Zone) o;
-		return Objects.equals(name, zone.name);
-	}
-	
-	
+    public void setTroops(int troops) {
+        this.troops = troops;
+    }
+
+    public void setNeighbours(ArrayList<Zone> neighbours) {
+        this.neighbours = neighbours;
+    }
+
+    public ArrayList<Zone> getNeighbours() {
+        return neighbours;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        Zone zone = (Zone) o;
+        return Objects.equals(name, zone.name);
+    }
+
+
 }
