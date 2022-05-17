4e60769b282b500c9320779f7a75b64e2a4a7638
braenyve <braenyve@students.zhaw.ch>
2022-05-17 07:48:04 +0200

    removed unnecessary comments and reformatted classes

--- lib/src/main/java/ch/zhaw/ovtycoon/Main.java
+++ lib/src/main/java/ch/zhaw/ovtycoon/Main.java
@@ -9,57 +9,29 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main extends Application{
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-            try {
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/StartWindow.fxml"));
-                Pane rootPane = fxmlLoader.load();
-                StartWindowController startWindowController = fxmlLoader.getController();
-                startWindowController.setParentSceneGraph(rootPane);
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/StartWindow.fxml"));
+            Pane rootPane = fxmlLoader.load();
+            StartWindowController startWindowController = fxmlLoader.getController();
+            startWindowController.setParentSceneGraph(rootPane);
 
-                Scene scene = new Scene(rootPane);
-                scene.getStylesheets().add(getClass().getClassLoader().getResource("start-window-styles.css").toExternalForm());
+            Scene scene = new Scene(rootPane);
+            scene.getStylesheets().add(getClass().getClassLoader().getResource("start-window-styles.css").toExternalForm());
 
-                primaryStage.setScene(scene);
+            primaryStage.setScene(scene);
 
-                primaryStage.setTitle("ÖV-Tycoon");
-                primaryStage.setMinWidth(600);
-                primaryStage.setMinHeight(400);
-                primaryStage.setResizable(false);
-                primaryStage.show();
+            primaryStage.setTitle("ÖV-Tycoon");
+            primaryStage.setMinWidth(600);
+            primaryStage.setMinHeight(400);
+            primaryStage.setResizable(false);
+            primaryStage.show();
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//            FXMLLoader fxmlLoader = new FXMLLoader(MapController.class.getClassLoader().getResource("StartWindow.fxml"));
-////            double scale = Screen.getPrimary().getBounds().getHeight() < 1000 ? 0.7d : 1.0d; //TODO
-//             Scene scene = new Scene(fxmlLoader.load(), 600,400);
-////            scene.getStylesheets().add(getClass().getClassLoader().getResource("map-styles.css").toExternalForm());
-////            Pane rootPane = fxmlLoader.load();
-////            StartWindowController startWindowController = fxmlLoader.getController();
-////            startWindowController.set
-//
-//            stage.setTitle("OV-Tycoon");
-//            stage.setScene(scene);
-//            stage.setResizable(false);
-//            stage.show();
-//        }
-//    }
+}
 
--- lib/src/main/java/ch/zhaw/ovtycoon/model/Fight.java
+++ lib/src/main/java/ch/zhaw/ovtycoon/model/Fight.java
@@ -4,79 +4,79 @@ import ch.zhaw.ovtycoon.Config;
 import ch.zhaw.ovtycoon.data.DiceRoll;
 
 public class Fight {
-	private Zone attackingZone;
-	private Zone defendingZone;
-	private Dice dice;
+    private Zone attackingZone;
+    private Zone defendingZone;
+    private Dice dice;
 
-	/**
-	 * Instantiates a new Fight object.
-	 * @param attackingZone		attacking Zone, can't be null
-	 * @param defendingZone		defending Zone, can't be null
-	 * 
-	 * @throws IllegalArgumentException	if one of the zones is null
-	 */
-	public Fight(Zone attackingZone, Zone defendingZone) {
-		if(attackingZone == null || defendingZone == null) {
-			throw new IllegalArgumentException();
-		}
-		this.attackingZone = attackingZone;
-		this.defendingZone = defendingZone;
-		dice = new Dice();
-	}
-	
-	/**
-	 * This fight methods executes the fight between the attacking zone and the
-	 * defending zone. The smaller amount of troops (arguments) is the number of
-	 * thrown dice per player/zone. The highest thrown number from the attacker is
-	 * compared to the highest thrown number of the defender. The attacker wins, if
-	 * his number is bigger than the defender. Then the defender loses one troop. If
-	 * the number from the attacker isn't higher, the attacker loses one troop. This
-	 * goes one as many times as the dice has been thrown.
-	 * 
-	 * @param attackingTroops Number of troops that the attacker sends to attack.
-	 *                        Must be positive and max amount is equals to
-	 *                        Config.MAX_THROWABLE_DICE and not bigger than troops
-	 *                        in attacking zone.
-	 * @param defendingTroops Number of troops that the defender sends to defend.
-	 *                        Must be positive and max amount is equals to
-	 *                        Config.MAX_THROWABLE_DICE and not bigger than troops
-	 *                        in defending zone.
-	 * @throws IllegalArgumentException if arguments are invalid.
-	 * @return rolledDie, a 2d array containing the defenders roll in the first column and the attackers in the second
-	 */
-	public DiceRoll fight(int attackingTroops, int defendingTroops) {
-		if (!isValidArgument(attackingTroops, true) || !isValidArgument(defendingTroops, false)) {
-			throw new IllegalArgumentException();
-		}
-		int[] attackingDice = dice.rollDice(attackingTroops);
-		int[] defendingDice = dice.rollDice(defendingTroops);
+    /**
+     * Instantiates a new Fight object.
+     *
+     * @param attackingZone attacking Zone, can't be null
+     * @param defendingZone defending Zone, can't be null
+     * @throws IllegalArgumentException if one of the zones is null
+     */
+    public Fight(Zone attackingZone, Zone defendingZone) {
+        if (attackingZone == null || defendingZone == null) {
+            throw new IllegalArgumentException();
+        }
+        this.attackingZone = attackingZone;
+        this.defendingZone = defendingZone;
+        dice = new Dice();
+    }
 
-		int lastAttackIndex = attackingTroops - 1;
-		int lastDeffendingIndex = defendingTroops - 1;
+    /**
+     * This fight methods executes the fight between the attacking zone and the
+     * defending zone. The smaller amount of troops (arguments) is the number of
+     * thrown dice per player/zone. The highest thrown number from the attacker is
+     * compared to the highest thrown number of the defender. The attacker wins, if
+     * his number is bigger than the defender. Then the defender loses one troop. If
+     * the number from the attacker isn't higher, the attacker loses one troop. This
+     * goes one as many times as the dice has been thrown.
+     *
+     * @param attackingTroops Number of troops that the attacker sends to attack.
+     *                        Must be positive and max amount is equals to
+     *                        Config.MAX_THROWABLE_DICE and not bigger than troops
+     *                        in attacking zone.
+     * @param defendingTroops Number of troops that the defender sends to defend.
+     *                        Must be positive and max amount is equals to
+     *                        Config.MAX_THROWABLE_DICE and not bigger than troops
+     *                        in defending zone.
+     * @return rolledDie, a 2d array containing the defenders roll in the first column and the attackers in the second
+     * @throws IllegalArgumentException if arguments are invalid.
+     */
+    public DiceRoll fight(int attackingTroops, int defendingTroops) {
+        if (!isValidArgument(attackingTroops, true) || !isValidArgument(defendingTroops, false)) {
+            throw new IllegalArgumentException();
+        }
+        int[] attackingDice = dice.rollDice(attackingTroops);
+        int[] defendingDice = dice.rollDice(defendingTroops);
 
-		int comparingDice = defendingTroops < attackingTroops ? defendingTroops : attackingTroops;
+        int lastAttackIndex = attackingTroops - 1;
+        int lastDeffendingIndex = defendingTroops - 1;
 
-		for (int i = 0; i < comparingDice; i++) {
-			if (attackingDice[lastAttackIndex - i] > defendingDice[lastDeffendingIndex - i]) {
-				defendingZone.decreaseZone(1);
-			} else {
-				attackingZone.decreaseZone(1);
-			}
-		}
-		
-		DiceRoll rolledDie = new DiceRoll(attackingDice, defendingDice, attackingTroops, defendingTroops);
-		return rolledDie;
-	}
+        int comparingDice = defendingTroops < attackingTroops ? defendingTroops : attackingTroops;
 
-	private boolean isValidArgument(int attackingTroops, boolean isAttacker) {
-		if (attackingTroops <= 0 || attackingTroops > Config.MAX_THROWABLE_DICE) {
-			return false;
-		}
-		int maxPossibleTroops = isAttacker ? attackingZone.getTroops() : defendingZone.getTroops();
-		if (maxPossibleTroops < attackingTroops) {
-			return false;
-		}
-		return true;
-	}
+        for (int i = 0; i < comparingDice; i++) {
+            if (attackingDice[lastAttackIndex - i] > defendingDice[lastDeffendingIndex - i]) {
+                defendingZone.decreaseZone(1);
+            } else {
+                attackingZone.decreaseZone(1);
+            }
+        }
+
+        DiceRoll rolledDie = new DiceRoll(attackingDice, defendingDice, attackingTroops, defendingTroops);
+        return rolledDie;
+    }
+
+    private boolean isValidArgument(int attackingTroops, boolean isAttacker) {
+        if (attackingTroops <= 0 || attackingTroops > Config.MAX_THROWABLE_DICE) {
+            return false;
+        }
+        int maxPossibleTroops = isAttacker ? attackingZone.getTroops() : defendingZone.getTroops();
+        if (maxPossibleTroops < attackingTroops) {
+            return false;
+        }
+        return true;
+    }
 
 }
